cdc9f5c1f89bd11fe2111a7bcf9fbbe339dd1022
YOHANESPRIMA <105904270+YOHANESPRIMA@users.noreply.github.com>
2022-05-20 13:23:37 +0700

    Add files via upload

---
+++ databese.js
@@ -0,0 +1,47 @@
+let produk= [];
+
+let produk_detail ={
+     foto : "https://www.google.com/url?sa=i&url=https%3A%2F%2Fsweetrip.id%2Fkuliner%2Fresep-jus-mangga%2F&psig=AOvVaw3KDUG9SYFvneTw5nU3lcMa&ust=1653097486681000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCKi9rZf67PcCFQAAAAAdAAAAABAD",
+     harga:15000,
+     kode :"BRG001",
+     NAMA :"JUS",
+     RATING:"5.0",
+     STOCK : 10
+
+};
+produk.push (produk_detail)
+
+
+produk_detail = {
+    foto:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fcookpad.com%2Fid%2Fresep%2F11729965-bakso-kenyal-dan-enak-daging-sapi-dan-ayam&psig=AOvVaw1BayZBP3w3kwjIWL6nBbN7&ust=1653097955069000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCNi30fb77PcCFQAAAAAdAAAAABAD",
+    harga:15000,
+    kode:"BRG002",
+    NAMA :"BAKSO",
+    RATING:"4,5",
+    STOCK:"20",
+
+};
+produk.push (produk_detail)
+
+produk_detail = {
+    foto:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fkreasimasakan.com%2Fsajian%2F3901-resep-bakwan-sayur-heci-bisa-manjain-lidah%2F&psig=AOvVaw1JG34nXiO1Bm0VFdKjzI4q&ust=1653098188364000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCKC79eT87PcCFQAAAAAdAAAAABAD",
+    harga:500, 
+    NAMA:"HECI",
+    KODE:"BRG003",
+    RATING:"5.0",
+    STOCK:"100",
+};
+produk.push (produk_detail)
+
+ produk_detail ={
+    foto:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.liputan6.com%2Fhealth%2Fread%2F3885799%2Fpermen-ternyata-bisa-meningkatkan-semangat-beraktivitas-makin-konsentrasi&psig=AOvVaw2tOAPaga28sQiFgnenlFmG&ust=1653098675725000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCJjkus3-7PcCFQAAAAAdAAAAABAD",
+    harga:100,
+    NAMA :"PERMEN",
+    KODE:"BRG004",
+    STOCK:"10",
+    RATING:"5.0",
+};
+produk.push (produk_detail)
+
+
+
